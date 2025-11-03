bc9ffdf5d18722ac978f0c0c51835251b13dfbda
Hugo Soares <hugos2000@protonmail.com>
2025-11-03 10:19:30 -0300

    Funcionalidades botões cena de derrota

---
+++ Scripts/derrota.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Jogar novamente
+func _on_btn_again_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/jogo.tscn")
+
+# Voltar para o menu
+func _on_btn_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
