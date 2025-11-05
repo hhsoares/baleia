07c43cd5c67b79e3ab23f20319c8dcc4ebdd3367
Hugo Soares <hugos2000@protonmail.com>
2025-11-05 10:08:54 -0300

    Consertado score final

--- Scripts/derrota.gd
+++ Scripts/derrota.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var txt_final_score: Label = $value_score
+
+func _process(_delta: float) -> void:
+	txt_final_score.text = str(Globals.score)
+
 # Jogar novamente
 func _on_btn_again_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/jogo.tscn")
